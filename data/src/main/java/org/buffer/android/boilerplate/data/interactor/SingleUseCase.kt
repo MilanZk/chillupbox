b8a7c526ff51c925f438da05bb04db26f7a10fd6
Xavier Mellado Esteban <xmellado@hiberus.com>
2018-12-04 10:12:48 +0100

    Use cases were cleaned and different reactive types were added.

--- data/src/main/java/org/buffer/android/boilerplate/data/interactor/SingleUseCase.kt
+++ data/src/main/java/org/buffer/android/boilerplate/data/interactor/SingleUseCase.kt
@@ -1,26 +1,26 @@
 package org.buffer.android.boilerplate.data.interactor
 
-import io.reactivex.Completable
+import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
 import org.buffer.android.boilerplate.data.executor.PostExecutionThread
 import org.buffer.android.boilerplate.data.executor.ThreadExecutor
 
 /**
- * Abstract class for a UseCase that returns an instance of a [Completable].
+ * Abstract class for a UseCase that returns an instance of a [Single].
  */
-abstract class CompletableUseCase<in Params> protected constructor(
+abstract class SingleUseCase<T, in Params> constructor(
         private val threadExecutor: ThreadExecutor,
         private val postExecutionThread: PostExecutionThread) {
 
     /**
-     * Builds a [Completable] which will be used when the current [CompletableUseCase] is executed.
+     * Builds a [Single] which will be used when the current [SingleUseCase] is executed.
      */
-    protected abstract fun buildUseCaseObservable(params: Params): Completable
+    protected abstract fun buildUseCaseObservable(params: Params? = null): Single<T>
 
     /**
      * Executes the current use case.
      */
-    fun execute(params: Params): Completable {
+    open fun execute(params: Params? = null): Single<T> {
         return this.buildUseCaseObservable(params)
                 .subscribeOn(Schedulers.from(threadExecutor))
                 .observeOn(postExecutionThread.scheduler)
